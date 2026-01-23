1726fc00238f5acc346493f7160daf6b48e215de
sameel prayurathanes <sameel.p@ku.th>
2026-01-23 14:17:26 +0700

    function comment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ function App() {
 
   const [todoList, setTodoList] = useState([]);
   const [newTitle, setNewTitle] = useState("");
+  const [newComments, setNewComments] = useState({});
 
   useEffect(() => {
     fetchTodoList();
@@ -60,6 +61,25 @@ function App() {
     }
   }
 
+  async function addNewComment(todoId) {
+    try {
+      const url = `${TODOLIST_API_URL}${todoId}/comments/`;
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ 'message': newComments[todoId] || "" }),
+      });
+      if (response.ok) {
+        setNewComments({ ...newComments, [todoId]: "" });
+        await fetchTodoList();
+      }
+    } catch (error) {
+      console.error("Error adding new comment:", error);
+    }
+  }
+
   async function deleteTodo(id) {
     const delete_api_url = `${TODOLIST_API_URL}${id}/`
     try {
@@ -83,6 +103,29 @@ function App() {
             <span className={todo.done ? "done" : ""}>{todo.title}</span>
             <button onClick={() => {toggleDone(todo.id)}}>Toggle</button>
             <button onClick={() => {deleteTodo(todo.id)}}>❌</button>
+            {(todo.comments) && (todo.comments.length > 0) && (
+              <>
+                <b>Comments:</b>
+                <ul>
+                  {todo.comments.map(comment => (
+                    <li key={comment.id}>{comment.message}</li>
+                  ))}
+                </ul>
+              </>
+            )}
+            <div className="new-comment-forms">
+              <input
+                type="text"
+                value={newComments[todo.id] || ""}
+                onChange={(e) => {
+                  const value = e.target.value;
+                  setNewComments({ ...newComments, [todo.id]: value });
+                }}
+              />
+            <button onClick={() => {addNewComment(todo.id)}}>Add Comment</button>
+            </div>
+            {/* ************** */}
+            
           </li>
         ))}
       </ul>
