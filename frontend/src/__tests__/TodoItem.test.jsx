da780b461f47493e8357e6735fbcd569ca69dfa1
sameel prayurathanes <sameel.p@ku.th>
2026-02-13 15:44:29 +0700

    add new comment

--- frontend/src/__tests__/TodoItem.test.jsx
+++ frontend/src/__tests__/TodoItem.test.jsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react'
-import { expect } from 'vitest'
+import { expect, vi } from 'vitest'
 import TodoItem from '../TodoItem.jsx'
+import userEvent from '@testing-library/user-event'
 
 const baseTodo = {             // ** TodoItem พื้นฐานสำหรับทดสอบ
   id: 1,
@@ -30,7 +31,7 @@ describe('TodoItem', () => {
     render(
       <TodoItem todo={todoWithComment} />
     );
-    expect(screen.getByText('Sampleกก Todo')).toBeInTheDocument();
+    expect(screen.getByText('Sample Todo')).toBeInTheDocument();
     expect(screen.getByText('First comment')).toBeInTheDocument();
     expect(screen.getByText('Another comment')).toBeInTheDocument();
     expect(screen.getByText(/2/)).toBeInTheDocument();
@@ -58,4 +59,36 @@ describe('TodoItem', () => {
     button.click();
     expect(onToggleDone).toHaveBeenCalledWith(baseTodo.id);
   });
+  it('makes callback to deleteTodo when delete button is clicked', () => {
+    const onDeleteTodo = vi.fn();
+    render(
+        <TodoItem
+        todo={baseTodo}
+        toggleDone={() => {}}
+        deleteTodo={onDeleteTodo}
+        addNewComment={() => {}}
+        />
+    );
+    const deleteButton = screen.getByText('❌');
+    deleteButton.click();
+    expect(onDeleteTodo).toHaveBeenCalledWith(baseTodo.id);
+    });
+  it('makes callback to addNewComment when a new comment is added', async () => {
+    const onAddNewComment = vi.fn();
+    render(
+        <TodoItem
+        todo={baseTodo}
+        toggleDone={() => {}}
+        deleteTodo={() => {}}
+        addNewComment={onAddNewComment}
+        />
+    );
+    const input = screen.getByRole('textbox');
+    await userEvent.type(input, 'New comment');
+    const button = screen.getByRole('button', { name: /add comment/i });
+    await userEvent.click(button);
+
+    expect(onAddNewComment).toHaveBeenCalledWith(baseTodo.id, 'New comment');
+    });
+
 });
