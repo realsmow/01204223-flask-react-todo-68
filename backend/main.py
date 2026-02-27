dd0bfa63f49fd98db333926ad3b851c141807253
sameel prayurathanes <sameel.p@ku.th>
2026-02-27 15:28:26 +0700

    secret

--- backend/main.py
+++ backend/main.py
@@ -5,14 +5,14 @@ import click
 from models import db, User
 from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
 from flask_jwt_extended import JWTManager
-
+import os
 from models import db, TodoItem, Comment
 
 app = Flask(__name__)
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todos.db'
-app.config['JWT_SECRET_KEY'] = 'fdsjkfjioi2rjshr2345hrsh043j5oij5545'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI','sqlite:///todos.db') 
+app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY','fdslkfjsdlkufewhjroiewurewrew')
 jwt = JWTManager(app)
 
 db.init_app(app)
